49f5318800d586ed40d38394e74a16afa3b1c51a
anubhav <anubhavadhikari15@gmail.com>
2023-11-13 20:01:00 +0530

    c1 - added all metadata microservice files

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+require('dotenv').config()
+
+const app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Your app is listening on port ' + port)
+});
